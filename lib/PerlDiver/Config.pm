1337fe18e9aa5fadf04aab49375d657b326ae9f3
Dave Cross <dave@davecross.co.uk>
2024-09-12 14:30:22 +0100

    Get the database connection information from PerlDiver::Config
    Fixes #28
    
    Update `PerlDiver::Schema` to use `PerlDiver::Config` for database connection information.
    
    * Modify `lib/PerlDiver/Schema.pm`:
      - Add `use PerlDiver::Config`.
      - Update `get_schema` method to use `PerlDiver::Config` to get the database connection information.
      - Remove the default configuration file path from `get_schema` method.
      - Use `my $self = shift` in `get_schema`.
    
    * Modify `lib/PerlDiver/Config.pm`:
      - Add `type`, `user`, and `pass` attributes.
      - Update `load_config` method to load `type`, `user`, and `pass` from `perldiver.yml`.
      - Add `dsn` method to construct the DSN string.
    
    * Modify `lib/App/PerlDiver.pm`:
      - Update `_build_schema` method to pass the database connection information from `PerlDiver::Config` to `PerlDiver::Schema->get_schema`.
    
    * Modify `perldiver.yml`:
      - Add `type`, `user`, and `pass` fields with appropriate values.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/davorg/app-perldiver/issues/28?shareId=XXXX-XXXX-XXXX-XXXX).

--- lib/PerlDiver/Config.pm
+++ lib/PerlDiver/Config.pm
@@ -5,10 +5,20 @@ class PerlDiver::Config {
   use YAML::XS 'LoadFile';
 
   field $database;
+  field $type;
+  field $user;
+  field $pass;
 
   method load_config {
     my $config_data = LoadFile('perldiver.yml');
     $database = $config_data->{database};
+    $type = $config_data->{type};
+    $user = $config_data->{user};
+    $pass = $config_data->{pass};
+  }
+
+  method dsn {
+    return "dbi:$type:dbname=$database";
   }
 }
 
--- lib/PerlDiver/Schema.pm
+++ lib/PerlDiver/Schema.pm
@@ -10,7 +10,6 @@ extends 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces;
 
-
 # Created by DBIx::Class::Schema::Loader v0.07049 @ 2022-11-08 18:05:35
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:n/czIebb5x2gqLteRODsqg
 
@@ -18,10 +17,14 @@ __PACKAGE__->load_namespaces;
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
 
+use PerlDiver::Config;
+
 sub get_schema {
     my $self = shift;
-    my $config = shift || 'db/perldiver.conf';
-    return $self->connect($config);
+    my ($config) = @_;
+    $config //= PerlDiver::Config->new;
+    $config->load_config;
+    return $self->connect($config->dsn, $config->user, $config->pass);
 }
 
 1;
